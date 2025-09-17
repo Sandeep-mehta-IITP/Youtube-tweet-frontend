be7bec35a9424e84fb53eea320acc0f3e9766ddc
Sandeep Mehta <sandeep797671@gmail.com>
2025-09-17 21:47:33 +0530

    Add utility functions for formatting timestamps, video durations, dates, and counts

---
+++ src/utils/helpers/formatFigure.js
@@ -0,0 +1,70 @@
+// Time stamp format
+export const formatTimestamp = (timestamp) => {
+  const now = new Date();
+  const date = new Date(timestamp);
+  const diffInSeconds = Math.floor((now - date) / 1000);
+
+  if (diffInSeconds < 60) {
+    return `${diffInSeconds} second${diffInSeconds !== 1 ? "s" : ""} ago`;
+  } else if (diffInSeconds < 3600) {
+    const minutes = Math.floor(diffInSeconds / 60);
+    return `${minutes} minute${minutes !== 1 ? "s" : ""} ago`;
+  } else if (diffInSeconds < 86400) {
+    const hours = Math.floor(diffInSeconds / 3600);
+    return `${hours} hour${hours !== 1 ? "s" : ""} ago`;
+  } else if (diffInSeconds < 604800) {
+    const days = Math.floor(diffInSeconds / 86400);
+    return `${days} day${days !== 1 ? "s" : ""} ago`;
+  } else if (diffInSeconds < 2592000) {
+    const weeks = Math.floor(diffInSeconds / 604800);
+    return `${weeks} week${weeks !== 1 ? "s" : ""} ago`;
+  } else if (diffInSeconds < 31536000) {
+    const months = Math.floor(diffInSeconds / 2592000);
+    return `${months} month${months !== 1 ? "s" : ""} ago`;
+  } else {
+    const years = Math.floor(diffInSeconds / 31536000);
+    return `${years} year${years !== 1 ? "s" : ""} ago`;
+  }
+};
+
+
+// video ki duration ko ek user friendly timing me format krna
+export const formatVideoDuration = (duration) => {
+  let totalSeconds = Math.floor(duration);
+
+  // let seconds = totalSeconds % 60 < 9 ? "0" + (totalSeconds % 60) : totalSeconds % 60
+  let seconds = String(totalSeconds % 60).padStart(2, "0");
+  let minutes = String(Math.floor(totalSeconds / 60)).padStart(2, "0");
+  let hours = Math.floor(totalSeconds / 3600);
+
+  if (hours > 0) {
+    return `${hours}:${minutes}:${seconds}`;
+  } else {
+    return `${minutes}:${seconds}`;
+  }
+};
+
+
+// date ko format krna -> DD / MM / YYYY
+export const formatDate = (timestamp) => {
+  const date = new Date(timestamp);
+  const days = date.getDate();
+  const months = date.getMonth() + 1;
+
+  const day = String(days).padStart(2, "0");
+  const month = String(months).padStart(2, "0");
+
+  return `${day}/${month}/${date.getFullYear()}`;
+};
+
+
+// word -> Subscriber, Like, Comment, Tweet ko singular and plural me format krna
+export const formatCount = (count, str = "Subscriber") => {
+  if (count === 0) {
+    return `No ${str.toLowerCase()}s yet`;
+  }
+
+  const word = count === 1 ? str : str + "s";
+
+  return `${count} ${word}`;
+};
