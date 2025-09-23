242686cbee613ce8942bb21104fdc0eb94a04d11
Sandeep Mehta <sandeep797671@gmail.com>
2025-09-23 16:03:21 +0530

    Enhance GuestComponent layout with improved styling and spacing

--- src/components/GuestPages/GuestComponent.jsx
+++ src/components/GuestPages/GuestComponent.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from "react";
 import LoginPopup from "../auth/LoginPopup";
 import { LogInIcon } from "lucide-react";
 
+
 const GuestComponent = ({
   icon,
   title = "Login to view this page",
@@ -11,9 +12,11 @@ const GuestComponent = ({
 }) => {
   const loginPopUpRef = useRef();
   return (
-    <section className="w-full flex items-center justify-center">
+    <section className="w-full mt-32 flex flex-col gap-4 items-center justify-center">
       {/* Icon */}
-      <div className="w-14 h-14 text-sky-400 rounded-full">{icon}</div>
+      <div className="w-20 h-20 text-sky-400 rounded-full">
+        {icon}
+      </div>
 
       {/* Title */}
       <p className="text-xl sm:text-2xl md:text-3xl font-semibold text-[#f6f5f6]">
@@ -21,7 +24,9 @@ const GuestComponent = ({
       </p>
 
       {/* description */}
-      <p className="text-lg font-normal text-white/80">{description}</p>
+      <p className="text-lg md:text-xl font-normal text-white/90">
+        {description}
+      </p>
 
       {/* Login Popup */}
       {guest && (
@@ -29,7 +34,7 @@ const GuestComponent = ({
           <LoginPopup route={route || ""} ref={loginPopUpRef} />
           <button
             onClick={() => loginPopUpRef.current.open()}
-            className="flex items-center gap-5 px-5 py-3 rounded-lg bg-slate-700 text-white"
+            className="flex items-center gap-5 px-5 py-3 rounded-lg bg-slate-700 hover:bg-gray-300 transition-colors duration-300 text-white hover:text-gray-900 font-semibold"
           >
             <LogInIcon className="w-5 h-5 text-sky-500" /> Log in
           </button>
