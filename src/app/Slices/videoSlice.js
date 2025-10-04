9ab20bd83915cde082d01cbd44bea70f03b81a52
Sandeep Mehta <sandeep797671@gmail.com>
2025-10-04 11:52:38 +0530

    Enhance video slice with publish, update, delete, and toggle functionality, including toast notifications

--- src/app/Slices/videoSlice.js
+++ src/app/Slices/videoSlice.js
@@ -1,6 +1,7 @@
 import { axiosInstance } from "@/API/axiosInstance";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const initialState = {
   data: null,
@@ -8,6 +9,99 @@ const initialState = {
   status: false,
 };
 
+export const publishVideo = createAsyncThunk(
+  "video/publishVideo",
+  async ({ data }, { signal, rejectWithValue }) => {
+    try {
+      const formData = new FormData();
+
+      for (const key in data) formData.append(key, data[key]);
+      formData.append("thumbnail", data.thumbnail[0]);
+      formData.append("videoFile", data.videoFile[0]);
+
+      const response = await axiosInstance.post("/videos/", formData, {
+        signal,
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      toast.success(response.data.message || "Video published successfully.");
+      return response.data.data;
+    } catch (error) {
+      if (axios.isCancel(error)) {
+        toast.error("Video publish cancelled");
+        return rejectWithValue("Cancelled by user");
+      }
+
+      console.log("FAILED TO PUBLSIH VIDEO", error.userMessage);
+      toast.error(error.userMessage || "Failed to publsih video.");
+      return rejectWithValue(error.userMessage);
+    }
+  }
+);
+
+export const updateVideo = createAsyncThunk(
+  "video/updateVideo",
+  async ({ videoId, data }, { rejectWithValue }) => {
+    try {
+      const formData = new FormData();
+      for (const key in data) formData.append(key, data[key]);
+      if (data.thumbnail) formData.append("thumbnail", data.thumbnail[0]);
+
+      const response = await axiosInstance.patch(
+        `/videos/${videoId}`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      toast.success(response.data.message || "Video updated successfully.");
+      return response.data.data;
+    } catch (error) {
+      console.log("FAILED TO UPDATE VIDEO", error.userMessage);
+      toast.error(error.userMessage || "Failed to update video.");
+      return rejectWithValue(error.userMessage);
+    }
+  }
+);
+
+export const deleteVideo = createAsyncThunk(
+  "video/deleteVideo",
+  async (videoId, { rejectWithValue }) => {
+    try {
+      const response = await axiosInstance.delete(`/videos/${videoId}`);
+      toast.success(response.data.message || "Video deleted Successfully.");
+      return response.data.data;
+    } catch (error) {
+      console.log("FAILED TO DELETE VIDEO", error.userMessage);
+      toast.error(error.userMessage || "Failed to delete video.");
+      return rejectWithValue(error.userMessage);
+    }
+  }
+);
+
+export const togglePublishStatus = createAsyncThunk(
+  "video/togglePublishStatus",
+  async (videoId, { rejectWithValue }) => {
+    try {
+      const response = await axiosInstance.patch(
+        `/videos/toggle/publish/${videoId}`
+      );
+      toast.success(
+        response.data.message || "Toggle publish video successfully."
+      );
+      return response.data.data;
+    } catch (error) {
+      console.log("FAILED TO TOGGLE PUBLSIH STATUS", error.userMessage);
+      toast.error(error.userMessage || "Failed to toggle publish status.");
+      return rejectWithValue(error.userMessage);
+    }
+  }
+);
 
 const videoSlice = createSlice({
   name: "video",
@@ -18,6 +112,63 @@ const videoSlice = createSlice({
       state.data = null;
       console.log("State.data:", state.data);
     },
+  },
+  extraReducers: (builder) => {
+    // Publish video
+    builder.addCase(publishVideo.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(publishVideo.fulfilled, (state, action) => {
+      state.loading = false;
+      state.data = action.payload;
+      state.status = true;
+    });
+    builder.addCase(publishVideo.rejected, (state) => {
+      state.loading = false;
+      state.status = false;
+    });
+
+    // update video
+    builder.addCase(updateVideo.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(updateVideo.fulfilled, (state, action) => {
+      state.loading = false;
+      state.data = action.payload;
+      state.status = true;
+    });
+    builder.addCase(updateVideo.rejected, (state) => {
+      state.loading = false;
+      state.status = false;
+    });
+
+    // delete video
+    builder.addCase(deleteVideo.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(deleteVideo.fulfilled, (state, action) => {
+      state.loading = false;
+      state.data = action.payload;
+      state.status = true;
+    });
+    builder.addCase(deleteVideo.rejected, (state) => {
+      state.loading = false;
+      state.status = false;
+    });
+
+    // toggle publish status
+    builder.addCase(togglePublishStatus.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(togglePublishStatus.fulfilled, (state, action) => {
+      state.loading = false;
+      state.data = action.payload;
+      state.status = true;
+    });
+    builder.addCase(togglePublishStatus.rejected, (state) => {
+      state.loading = false;
+      state.status = false;
+    });
   },
 });
 
